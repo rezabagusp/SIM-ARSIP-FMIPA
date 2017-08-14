f62cae8081549456e2f5bc98641acef7636f23b3
Aslam Abdurrohim <aslamabdurrohim@gmail.com>
2017-08-14 09:21:03 +0700

    edit dataform

--- api/models/penerima.model.js
+++ api/models/penerima.model.js
@@ -4,7 +4,7 @@ var Jabatan = sequelize.import(__dirname + "/../models/jabatan.model");
 module.exports = function(sequelize, DataType){
 	return sequelize.define('penerima',{
 		nama_penerima: DataType.STRING,
-        jabatan_penerima: { type: DataType.INTEGER, references: { model: Jabatan, key: 'id' }, defaultValue: null },
-        email_penerima: DataType.STRING
+        email_penerima: DataType.STRING,
+        jabatan_id: { type: DataType.INTEGER, references: { model: Jabatan, key: 'id' }, defaultValue: null }
     });
 }

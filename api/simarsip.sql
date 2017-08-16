25e0b84bfb487ff8c23dedf3d530420508b03c0a
Aslam Abdurrohim <aslamabdurrohim@gmail.com>
2017-08-16 10:28:16 +0700

    simarsip sql

--- api/simarsip.sql
+++ api/simarsip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 16, 2017 at 03:51 AM
+-- Generation Time: Aug 16, 2017 at 05:26 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 

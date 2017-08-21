a0f3d47ea2cb7a12953563ca88c6c35f865af94f
parhan zikkry padly <parhanzikkry@gmail.com>
2017-08-21 12:12:20 +0700

    repaire mailer.js

--- api/mailer.js
+++ api/mailer.js
@@ -1,19 +1,19 @@
 'use strict'
 const nodemailer = require('nodemailer')
 var sequelize = require(__dirname + '/connection')
-var Surat = sequelize.import(__dirname + '/models/surat.model');
-var Lampiran = sequelize.import(__dirname + '/models/lampiran.model');
-var Staff = sequelize.import(__dirname + '/models/staff.model');
-var Perihal = sequelize.import(__dirname + '/models/perihal.model');
-var Surat_masuk_penerima = sequelize.import(__dirname + '/models/surat_masuk_penerima.model');
-var Surat_masuk_pengirim = sequelize.import(__dirname + '/models/surat_masuk_pengirim.model');
+const Lampiran = sequelize.import(__dirname + '/models/lampiran.model')
+const Surat = sequelize.import(__dirname + '/models/surat.model')
+const Staff = sequelize.import(__dirname + '/models/staff.model')
+var Perihal = sequelize.import(__dirname + '/models/perihal.model')
+var Surat_masuk_penerima = sequelize.import(__dirname + '/models/surat_masuk_penerima.model')
+var Surat_masuk_pengirim = sequelize.import(__dirname + '/models/surat_masuk_pengirim.model')
 
-Surat.belongsTo(Perihal, {foreignKey: 'perihal_id'});
-Surat.hasMany(Surat_masuk_penerima, {foreignKey: 'surat_id'});
+Surat.belongsTo(Perihal, {foreignKey: 'perihal_id'})
+Surat.hasMany(Surat_masuk_penerima, {foreignKey: 'surat_id'})
 Lampiran.belongsTo(Surat, {foreignKey: 'surat_id'})
-Surat.hasOne(Surat_masuk_pengirim, {foreignKey: 'surat_id'});
-Surat_masuk_penerima.belongsTo(Staff, {foreignKey: 'staff_id'});
-Staff.hasMany(Surat_masuk_penerima, {foreignKey: 'staff_id'});
+Surat.hasOne(Surat_masuk_pengirim, {foreignKey: 'surat_id'})
+Surat_masuk_penerima.belongsTo(Staff, {foreignKey: 'staff_id'})
+Staff.hasMany(Surat_masuk_penerima, {foreignKey: 'staff_id'})
 
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({
@@ -25,60 +25,49 @@ var transporter = nodemailer.createTransport({
         pass: 'akangaep10'    // put your email password here
     }
 });
-
-var sender = '"SIMARSIP FMIPA" <miqdadfawwaz95@gmail.com>';
-var receivers = [];
-var subject = "";
-var attachments = [];
-var html = "";
-
-var send = function(res) {
-    // setup email data with unicode symbols
-    var mailOptions = {
-        from: sender, // sender address
-        to: receivers, // list of receivers
-        subject: subject, // Subject line
-        html: html, // html body
-        attachments: attachments
+var misal = [{id:1}, {id:2}, {id:3}]
+class Mailer{
+    constructor() {
+        this.sender = '"SIMARSIP FMIPA" <miqdadfawwaz95@gmail.com>'
+        this.recievers = []
+        this.subject = 'Surat Masuk di SIMARSIP'
+        this.attachments = []
+        this.html = ''
     }
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(err, info) {
-        if (err) {
-            res.json({status: false, message: 'Tambah surat gagal!', err_code: 400, err: err});
-        } else {
-            res.json({status: true, message: 'Tambah surat berhasil!', data: info});
-        }
-    })
-}
-
-function Mailer() {
-    this.sendSurat = function(id, status, res) {
-        subject += "Surat Masuk | SIMARSIP";
-        html += "<p>Ada surat masuk untuk Anda di SIMARSIP. Silahkan cek seluruh lampiran email ini untuk melihat isi suratnya. Surat fisik dapat diambil di TU. Terima kasih.</p><br>";
+    SetHtml(surat, status) {
+        if (status > 0) {
+                this.html += `
+                <p>Surat berikut adalah surat yang didisposisikan kepada Anda.</p>
+                <br>
+                `
+            }
+        this.html += `
+            <p>Nomor: ` + surat.dataValues.nomor_surat + `/` + surat.dataValues.unit_kerja_surat + `/` + surat.dataValues.hal_surat + `/` + surat.dataValues.tahun_surat + `</p>
+            <p>Perihal: ` + surat.dataValues.perihal.dataValues.nama_perihal + `</p>
+            <p>Pengirim: ` + surat.dataValues.surat_masuk_pengirim.dataValues.nama_pengirim + `</p>
+            <p>Tanggal surat: ` + surat.dataValues.tanggal_surat + `</p>
+            <p>Tingkat kepentingan: ` + surat.dataValues.kepentingan_surat + `</p>
+            <p>Sifat: ` + surat.dataValues.sifat_surat + `</p><br>
+            <p>Catatan: ` + surat.dataValues.keterangan_surat + `</p><br>
+        `
+    }
+    SendSuratWithLampiran(id, status, res) {
+        /*Get surat and lampiran first*/
+        console.log(misal)
         Surat
             .findOne({
                 where: {
                     id: id
                 },
                 include: [{
-                    model: Surat_masuk_pengirim
-                }, {
                     model: Perihal
+                },{
+                    model: Surat_masuk_pengirim
                 }]
             })
-            .then(function(surat) {
-                if (status > 0) {
-                    html += "<p>Surat berikut adalah surat yang didisposisikan kepada Anda.</p><br>";
-                }
-                html += "<p>Nomor: " + surat.dataValues.nomor_surat + "/" + surat.dataValues.unit_kerja_surat + "/" + surat.dataValues.hal_surat + "/" + surat.dataValues.tahun_surat + "</p><br>";
-                html += "<p>Perihal: " + surat.dataValues.perihal.dataValues.nama_perihal + "</p><br>"
-                html += "<p>Pengirim: " + surat.dataValues.surat_masuk_pengirim.dataValues.nama_pengirim + "</p><br>";
-                html += "<p>Tanggal surat: " + surat.dataValues.tanggal_surat + "</p><br>";
-                html += "<p>Tingkat kepentingan: " + surat.dataValues.kepentingan_surat + "</p><br>";
-                html += "<p>Sifat: " + surat.dataValues.sifat_surat + "</p><br>";
-                html += "<p>Catatan: " + surat.dataValues.keterangan_surat + "</p><br>";
-                attachments.push({filename: surat.dataValues.file_surat, path: __dirname + '/public/uploads/surat/' + surat.dataValues.file_surat});
+            .then((surat) => {
+                this.attachments.push({filename: surat.dataValues.file_surat, path: __dirname + '/public/uploads/surat/' + surat.dataValues.file_surat})
+                this.SetHtml(surat, status)
                 Lampiran
                     .findAll({
                         where: {
@@ -86,12 +75,11 @@ function Mailer() {
                         },
                         attributes: ['file_lampiran']
                     })
-                    .then(function(lampiran) {
-                        if (lampiran !== 0) {
-                            for(var i = 0; i < lampiran.length; i++) {
-                                attachments.push({filename: lampiran[i].dataValues.file_lampiran, path: __dirname + '/public/uploads/lampiran/' + lampiran[i].dataValues.file_lampiran})
-                            }
+                    .then((lampiran) => {
+                        for(let i=0; i<lampiran.length; i++) {
+                            this.attachments.push({filename: lampiran[i].dataValues.file_lampiran, path: __dirname + '/public/uploads/lampiran/' + lampiran[i].dataValues.file_lampiran})
                         }
+                        // this.send(data, res)
                         Staff
                             .findAll({
                                 include: [{
@@ -102,14 +90,32 @@ function Mailer() {
                                     }
                                 }]
                             })
-                            .then(function(staff) {
-                                for(var i = 0; i < staff.length; i++) {
-                                    receivers.push(staff[i].dataValues.email_staff)
+                            .then((staff) => {
+                                for(let i=0; i<staff.length; i++) {
+                                    this.recievers.push(staff[i].dataValues.email_staff)
                                 }
-                               send(res)
+                                this.send(res)
                             })
                     })
             })
+    }
+    send(res) {
+        // setup email data with unicode symbols
+        var mailOptions = {
+            from: this.sender, // sender address
+            to: this.recievers, // list of receivers
+            subject: this.subject, // Subject line
+            html: this.html, // html body
+            attachments: this.attachments
+        }
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function(err, info) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        })
     }
 }
 
